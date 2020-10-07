fbfa73cadf7a8da08fcef1e71f3151593f596e52
Courtney Robison <courtneyrobison3@gmail.com>
2020-10-06 22:19:41 -0700

    started code for determining numner of months

---
+++ PyBank/main.py
@@ -0,0 +1,20 @@
+import os
+
+import csv
+
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    months = len(list(csvreader))
+    print(months)
+    
+
+
