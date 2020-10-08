6c7723fee75207b41e0c0952c02b7c11739e04f2
Courtney Robison <courtneyrobison3@gmail.com>
2020-10-07 18:09:04 -0700

    fixed path to csv so it works!

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,7 +2,7 @@ import os
 
 import csv
 
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
 with open(csvpath) as csvfile:
 
@@ -10,6 +10,7 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     print(csvreader)
+    
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
