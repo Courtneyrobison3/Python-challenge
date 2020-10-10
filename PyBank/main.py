6eda4eb8b91908313e96b86574ddbe17904abdb5
Courtney Robison <courtneyrobison3@gmail.com>
2020-10-09 19:03:03 -0700

    added for loop to compute number of months and net total profits

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,8 +14,34 @@ with open(csvpath) as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
-    months = len(list(csvreader))
-    print(months)
+    month_count =0
+    net_total= 0
+
+    for row in csvreader:
+        month_count = month_count + 1 
+        net_total = net_total + int(row[1])
+    print(month_count)
+    print(net_total)
+
+        
+
+
+
     
+# # Specify the file to write to
+# output_path = os.path.join("..", "analysis", "results.csv")
+
+
+# # Open the file using "write" mode. Specify the variable to hold the contents
+# with open(output_path, 'results') as csvfile:
+
+#     # Initialize csv.writer
+#     csvwriter = csv.writer(csvfile, delimiter=',')
+
+#     # Write the first row (column headers)
+#     csvwriter.writerow(['First Name', 'Last Name', 'SSN'])
+
+#     # Write the second row
+#     csvwriter.writerow(['Caleb', 'Frost', '505-80-2901'])
 
 
