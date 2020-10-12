eeb4db3d18b312691172b0fb3c80bfd182a98706
Courtney Robison <courtneyrobison3@gmail.com>
2020-10-12 13:41:45 -0700

    set and filled candidates dictionary

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,21 +3,35 @@ import os
 import csv
 
 csvpath = os.path.join('Resources', 'election_data.csv')
-
+#create null dictionary to store candidate names
+candidate_name = {}
 with open(csvpath) as csvfile:
-
-    # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
-
+    #CSV reader specifies delimiter and variable that holds contents 
+    csvreader = csv.reader(csvfile,delimiter=',')
     print(csvreader)
 
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
-
+#define variables
     votes = 0
+    candidates = {}
+    
+    #determine the number of votes cast by counting t
+
 # determine the number of votes cast by counting the number of voter IDs
     for row in csvreader:
         votes = votes + 1
 
-       
-    print(int(votes))
+    
+        
+        #loop through and find the 
+
+        if row[2] not in candidates:
+           # candidates[row[2]] = 1
+            candidates.update({row[2] : 1})
+        else:
+            candidates[row[2]] += 1
+             
+    
+print(candidates)
+print(int(votes))
